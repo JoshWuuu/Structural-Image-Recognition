4d3cc048acc3afac9263ca40c6ff40422b2575d3
Langston Nashold <lgnashold@gmail.com>
2020-11-08 23:48:21 -0600

    Basic hyperparam tuning

---
+++ hyperparameter_tuning.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+from model import create_model
+from util import *
+
+def mkdir_if_not_exist(dir_path):
+    try:
+        os.mkdir(dir_path)
+    except FileExistsError:
+        pass
+
+def main():
+    # train_x, train_y, val_x, val_y = get_data(0.9)
+    train_x = np.zeros((1000, 224, 224, 3))
+    train_y = np.zeros((1000, 1))
+    val_x = train_x
+    val_y = train_y
+
+    input_shape = train_x.shape[1:]
+    num_class = train_y.shape[1]
+
+    generator = keras.preprocessing.image.ImageDataGenerator(
+        data_format='channels_last'
+    )
+
+    history_dir = "/home/langston/history"
+    model_dir = "/home/langston/models"
+    mkdir_if_not_exist(history_dir)
+    mkdir_if_not_exist(model_dir)
+    history_dir += "/"
+    model_dir += "/"
+
+    results_filename = '/home/langston/results.csv'
+
+    columns = ['Frozen Layers', 'Initial LR', 'LR Decay', 'Max Epochs', 'Train Loss', 'Train Accuracy', 'Train AUC', 'Val Loss', 'Val Accuracy', 'Val AUC']
+    results = pd.DataFrame(columns = columns)
+
+    for i in range(30):
+        # INITIALIZE HYPERPARAMS
+        # Model has 177 layers
+        frozen_layers = np.random.randint(100, 176)
+        exp = np.random.uniform(1, 5)
+        initial_lr = 10 ** (- exp)
+        decay_rate_exp = np.random.uniform(1, 2.5)
+        lr_decay = 1 - 10 ** (-decay_rate_exp)
+        max_epochs = np.random.randint(10, 14)
+
+        # Create Model
+        model = create_model(frozen_layers, input_shape, num_class)
+
+        # Fit Model
+        model, history = compile_and_fit(model, generator, train_x, train_y, val_x, val_y, max_epochs)
+
+        # Save everything useful
+        save_history(history_dir + str(i) + ".csv")
+        save_model(model_dir + str(i))
+
+        train_perf = model.evaluate(train_x, train_y, verbose=1)
+        val_perf = model.evaluate(val_x, val_y, verbose=1)
+        hyper_param_arr = [frozen_layers, initial_lr, lr_decay, max_epochs]
+        results.loc[i] = hyper_param_arr + train_perf + val_perf
+
+        with open(results_filename, 'w') as f:
+            results.to_csv(f)
+
+
+
+
+if __name__ == '__main__':
+    main()
